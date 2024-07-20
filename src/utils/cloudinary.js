b0500d90072adc4c49bc1491eb1856d704f4a39f
Ayesha <www.ayeshapyari123@gmail.com>
2024-07-20 19:22:38 +0500

    handling file configuration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_KEY_SECRET, // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    //upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto", //detect urself the type
+    });
+
+    console.log("file has uploaded", response.url);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath); //remove locally saved temp file
+
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
