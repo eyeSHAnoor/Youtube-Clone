e65d0bd0630029f7846f10d54d61228019978130
Ayesha <www.ayeshapyari123@gmail.com>
2024-07-20 13:22:05 +0500

    DB connect successfully

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./env",
+});
+
+import connectDB from "./db/db.js";
+import express from "express";
+const app = express();
+
+connectDB();
