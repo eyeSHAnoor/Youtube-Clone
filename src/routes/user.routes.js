a0a706cb9ef28b7dc94c64ddb799459f7c913681
Ayesha <www.ayeshapyari123@gmail.com>
2024-07-21 17:31:33 +0500

    login and logout controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logOutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
@@ -16,5 +21,11 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
+router.route("/login").post(loginUser);
+
+//secured route
+
+router.route("/logout").post(verifyJWT, logOutUser);
 
 export default router;
